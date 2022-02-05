334c6b58f27d4b7d82141926d663a865c6434f36
Eric Walker <eric.walker@shopify.com>
2022-02-04 22:40:29 -0500

    More play route finding

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+
+gem "priority_queue_cxx", "~> 0.3.4"
---
+++ route.rb
@@ -0,0 +1,79 @@
+require 'csv'
+require 'fc'
+require 'pry'
+
+def read_edges
+  edges = []
+  CSV.open("edges.csv").each do |csv|
+    edges << Edge.new(csv[0], csv[1], csv[2].to_f, [csv[3]])
+  end
+
+  Graph.new(edges)
+end
+
+Edge = Struct.new(:source, :destination, :cost, :line)
+
+class Graph
+  attr_reader :edge_table
+  def initialize(edges)
+    @edges = edges
+    build_edge_table
+  end
+
+  def find_edges(node)
+    @edge_table[node]
+  end
+
+  def build_edge_table
+    @edge_table = Hash.new { |h, k| h[k] = [] }
+    @edges.each do |e|
+      @edge_table[e.source] << e
+    end
+  end
+end
+
+
+# dijkstra(read_edges, "1", "260")
+# crazy example, it's cheaper to swith to line 6 than to go straight from 1 to 12
+# should go straight from line 1 to line 12
+# seems to still be a bug; with a super high switch cost, this should flip back to to the two hop transit
+
+def dijkstra(graph, source, destination)
+  switch_cost = 20
+  dist = {}
+  dist[source] = 0
+  visited = {}
+  q = FastContainers::PriorityQueue.new(:min)
+  graph.find_edges(source).map do |e|
+    q.push(e, e.cost)
+  end
+  visited[source] = []
+
+  q.pop_each do |e, cost|
+    if e.destination == destination
+      binding.pry
+      return [cost, visited[e.source]+[e]]
+    end
+    if visited.key?(e.destination) && cost > dist[e.destination]
+      next
+    end
+    dist[e.destination] = cost
+    visited[e.destination] = visited[e.source] + [e]
+
+    graph.find_edges(e.destination).map do |next_e|      
+      line_switch = if e.line == next_e.line
+        0
+      else
+        switch_cost
+      end
+
+      q.push(next_e, next_e.cost + line_switch + cost)
+    end
+  end
+
+  return nil
+end
+
+def print_q(q)
+  q.each { |z, c| puts "#{z}=>#{c}" }
+end
